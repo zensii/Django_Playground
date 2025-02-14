108ba6b9513fc5802e25f0c7ba014548799bf4fe
svetl <svetlin.sii@gmail.com>
2025-02-14 10:45:19 +0200

    added a new project

---
+++ DJ_Templates_Basics/DJ_Templates_Basics/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DJ_Templates_Basics project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJ_Templates_Basics.settings')
+
+application = get_asgi_application()
